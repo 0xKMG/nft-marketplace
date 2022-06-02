b2edf37629f387fc2688abb0048c4eb53bd1c349
Miles Kwok <83229800+KMGwei@users.noreply.github.com>
2022-06-02 15:40:20 +0800

    first commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Head from 'next/head';
+import Image from 'next/image';
+
+const Home = () => (
+  <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+);
+
+export default Home;
